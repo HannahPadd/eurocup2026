9bffae5459ba2645059285c68765d7390ee22914
Hannah <57945088+HannahPadd@users.noreply.github.com>
2026-03-10 19:26:28 +0100

    Update allowedHosts to use HTTPS for API

--- tournament_viewer/vite.config.ts
+++ tournament_viewer/vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   server: {
     port: 5173,
     host: "0.0.0.0",
-    allowedHosts: ["api.itgeurocup.com"],
+    allowedHosts: ["https://api.itgeurocup.com"],
     watch: {
       usePolling: true,
     },
