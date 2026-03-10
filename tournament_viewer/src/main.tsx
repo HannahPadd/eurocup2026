3ac52d85fbceb81b99d57e497d177f7019490923
Hannah Lynn Lindrob <hannahlindrob@outlook.com>
2026-03-10 19:56:54 +0100

    update main to use env var

--- tournament_viewer/src/main.tsx
+++ tournament_viewer/src/main.tsx
@@ -6,7 +6,7 @@ import axios from "axios";
 import { AuthProvider } from "./context/AuthContext.tsx";
 
 
-axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
