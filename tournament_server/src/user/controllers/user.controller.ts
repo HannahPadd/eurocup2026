31022d77d7343d4ac9ba6de67fdd7410184b2f72
HannahPadd <hannah@lindrob.nl>
2026-03-15 14:29:44 +0100

    Route permissions update

--- tournament_server/src/user/controllers/user.controller.ts
+++ tournament_server/src/user/controllers/user.controller.ts
@@ -5,17 +5,20 @@ import { UserService } from '../services';
 import { CreateUserPlayerDto, UpdateUserPlayerDto } from '../dtos';
 
 import { Account } from '@persistence/entities';
+import { Public } from '@auth/public.decorator';
 
 @Controller('user')
 export class UserController {
     constructor(private readonly service: UserService) { }
 
     //TODO: avoid duplicate registrations
+    @Public()
     @Post()
     async create(@Body(new ValidationPipe()) dto: CreateUserPlayerDto) {
         return await this.service.create(dto);
     }
-
+    
+    @Public()
     @Get('registration-prefill')
     async getRegistrationPrefill(@Query('gamerTag') gamerTag: string) {
         if (!gamerTag || !gamerTag.trim()) {
